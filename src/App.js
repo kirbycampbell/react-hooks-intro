f3f7eaed74296a13c122b2fc81b73bcfa792f954
kirbycampbell <jkirbycampbell@gmail.com>
2019-01-21 16:55:58 -0800

    Styling Added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,88 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 export default function App() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("react hooks");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const searchInputRef = useRef();
 
   useEffect(() => {
-    axios
-      .get("http://hn.algolia.com/api/v1/search?query=reacthooks")
-      .then(response => {
-        console.log(response.data);
-        setResults(response.data.hits);
-      });
+    getResults();
   }, []);
-  // The above empty array, keeps it from refreshing constantly
+  //This keeps every keystroke updating the query if query is placed in the empty array
+
+  const getResults = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setResults(response.data.hits);
+    } catch (err) {
+      setError(err);
+    }
+
+    setLoading(false);
+  };
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  };
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+    //Focus brings the cursor to the newly cleared input form.  Pretty cool!
+  };
+
   return (
-    <>
-      <ul>
-        {results.map(result => (
-          <li key={result.objectID}>
-            <a href={result.url}>{result.title}</a>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/c0c"
+        alt="react logo"
+        className="float-right h-12"
+      />
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form onSubmit={handleSearch} className="mb-2">
+        <input
+          value={query}
+          type="text"
+          onChange={event => setQuery(event.target.value)}
+          ref={searchInputRef}
+          className="border p-1 rounded"
+        />
+        <button type="submit" className="bg-orange rounded m-1 p-1">
+          Search
+        </button>
+        <button
+          type="button"
+          onClick={handleClearSearch}
+          className="bg-teal text-white p-1 rounded"
+        >
+          Clear
+        </button>
+      </form>
+      {loading ? (
+        <div className="font-bold text-orange-dark">Loading results...</div>
+      ) : (
+        <ul className="list-reset leading-normal">
+          {results.map(result => (
+            <li key={result.objectID}>
+              <a
+                href={result.url}
+                className="text-indigo-dark hover:text-indigo-darkest"
+              >
+                {result.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
   );
 }
